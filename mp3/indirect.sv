eb11933fc061cb6d38ada6b0f0ebc093c88cff9b
Colin Lord <cdlord2@illinois.edu>
2016-11-06 04:25:00 -0600

    Indirect module changes

--- mp3/indirect.sv
+++ mp3/indirect.sv
@@ -25,56 +25,54 @@ module indirect
 /* Reset Control */
 enum int unsigned {
 	 idle,
-    i1,
-	 i2
+    i1
 } state, next_state;
+logic finished_indirect, finished_indirect_next;
+lc3b_word addr, addr_next;
 always_comb
 begin
 	case (state)
+		default : begin
+				addr_next = addr;
+		end
 		idle : begin
-				P_mem_resp = D_mem_resp;
-				D_mem_read = P_mem_read;
-				D_mem_write = P_mem_write;
+				P_mem_resp = D_mem_resp & !(indirect & !finished_indirect);
+				D_mem_read = P_mem_read | indirect;
+				D_mem_write = P_mem_write & !indirect;
 				D_mem_address = P_mem_address;
 				D_mem_wdata = P_mem_wdata;
 				P_mem_rdata = D_mem_rdata;
+				addr_next = D_mem_rdata;
 				mem_byte_enable = P_mem_byte_enable;
-				if((P_mem_read | P_mem_write) & indirect)
+				finished_indirect_next = 0;
+				if(D_mem_resp & indirect)
 					next_state <= i1;
 				else
 					next_state <= idle;
 		end
 		i1 : begin
-				P_mem_resp = 0;
-				D_mem_read = 1;
-				D_mem_write = 0;
-				D_mem_address = P_mem_address;
-				D_mem_wdata = P_mem_wdata;
-				P_mem_rdata = D_mem_rdata;
-				mem_byte_enable = P_mem_byte_enable;
-				if(D_mem_resp)
-					next_state <= i2;
-				else
-					next_state <= i1;
-		end
-		i2 : begin
 				P_mem_resp = D_mem_resp;
 				D_mem_read = P_mem_read;
 				D_mem_write = P_mem_write;
-				D_mem_address = D_mem_rdata;
+				D_mem_address = addr;
+				addr_next = addr;
 				D_mem_wdata = P_mem_wdata;
 				P_mem_rdata = D_mem_rdata;
 				mem_byte_enable = P_mem_byte_enable;
+				finished_indirect_next = 1;
 				if(D_mem_resp)
 					next_state <= idle;
 				else
-					next_state <= i2;
+					next_state <= i1;
 		end
+
 	endcase
 end
 always_ff @(posedge clk)
 begin
 	state <= next_state;
+	finished_indirect <= finished_indirect_next;
+	addr <= addr_next;
 end
 
 endmodule : indirect
