27704cf128d6088ad153ac18d83e7091b4e57423
Colin Lord <cdlord2@illinois.edu>
2016-11-28 23:36:25 -0600

    Properly initialize array to weakly not taken

--- mp3/array.sv
+++ mp3/array.sv
@@ -22,12 +22,11 @@ initial
 
 begin
 
-    for (int i = 0; i < $size(data); i = i + 2)
+    for (int i = 0; i < $size(data); i++)
 
     begin
 
-        data[i] = 1'b0;
-        data[i+1] = 1'b1;
+        data[i] = 2'b01;
 
     end
 
